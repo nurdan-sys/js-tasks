07a466ead644ccc35b5aa888e0cd487f1a47fd30
nurdan-sys <nurdan.alpbek@gmail.com>
2023-03-08 09:51:47 -1100

    jas-task

---
+++ script/script.js
@@ -0,0 +1,81 @@
+// let a = "hello world"
+// let b = "great day"
+// let c = b.slice(0,b.indexOf(" day"))
+// let d = a.slice(a.indexOf(" "))
+// console.log(c + d)
+
+// let a = 'lorem ipsum dolor'
+// let b = a.slice(0, 15)
+// let c = a.slice(15, 17).replace('o', 'a')
+// console.log(b+c)
+
+// let a = 'hello'
+// let b = 'Ivan'
+// let c = a.slice(a.indexOf(''), 1) . toUpperCase() + a.slice(1)
+// let d = b.slice(b.indexOf(''), 1) . toLowerCase() + b.slice(1)
+// console.log(c + ' ' + d)
+
+// let a = "hello world"
+// console.log(a.replaceAll('l','k'))
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.slice(18, 22)
+// let c = a.slice(a.indexOf(''), 18) + a.slice(22, 27)
+// console.log(b + c)
+
+// let a = 'children'
+// let b = a.slice(a.indexOf(''), 7). toUpperCase() + a.slice(7)
+// console.log(b)
+
+// let a = 'boss'
+// let b = a.slice(a.indexOf(''), 3)
+// let c = b.replace('o', 's') + a.slice(1, 2)
+// console.log(c)
+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+// console.log(b)
+
+// let a = 'sunday'
+//
+// console.log(a.indexOf('d'))
+
+// let a = 'hello'
+// let b = a.replace('l', 'k')
+//
+// console.log(b.replace('l', 's'))
+
+// let a = 'user'
+// let b = 'new'
+// let c = a.slice(0, 2)
+//
+// console.log(b+' '+ c)
+
+// let a = 'monday'
+// let b = 'day'
+// let c = a.slice(0, 3)
+// let d = a.slice(3, 6)
+//
+// console.log(b + d + c)
+
+// let a = 'HELLO'
+// let b = a.slice(0, 1).toLowerCase() + a.slice(1, 2) + a.slice(2, 3).toLowerCase() + a.slice(3, 4) + a.slice(4, 5).toLowerCase()
+//
+//
+// console.log(b)
+
+// let a = 5
+// let b = '400'
+// let c = a.toString()
+// let d = b.replace('0', a)
+// console.log(d)
+
+
+// let a = 'monkey'
+// let b = 'banana'
+// let d = a.slice(0, 1) + b.slice(1, 4) + a.slice(3, 4)
+//
+// console.log(d)
+
+
+
